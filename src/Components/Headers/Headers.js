3e0d04aff32e42fb0771bfafe684ee0a2edd961f
subirdas29 <dasjoy104@gmail.com>
2022-12-09 01:28:20 +0600

    edited

--- src/Components/Headers/Headers.js
+++ src/Components/Headers/Headers.js
@@ -18,7 +18,7 @@ const Headers = () => {
         <div>
             <div className={navbar? 'navbar active': 'navbar'}>
   <div>
-    <div className="dropdown">
+    <div className="dropdown "style={{backgroundColor: "transparent" }}>
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
